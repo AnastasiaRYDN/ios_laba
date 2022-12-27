fc3458c001a1a2db15888c302a97ddd207fd64c8
Diana <dvkrasnova19@gmail.com>
2022-12-27 22:32:41 +0300

    first commit

--- lab1/ViewController.swift
+++ lab1/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onButtonClick(_ sender: Any) {
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        label.text = dateFormatter.string(from: currentDate)
+    }
 }
 
